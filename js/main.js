45418464c7ef9ca3a42b3191d0492c310ce8c6c1
ColeVanderkolk <vanderkolkcb@gmail.com>
2024-10-18 11:17:58 -0700

    AHHHHHHH

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 function toggleNightMode() {
     const themeStylesheet = document.getElementById('themeStylesheet');
+    const video = document.getElementById('backgroundVideo');
+    const image = document.getElementById('nighttimeImage');
 
     if (themeStylesheet.getAttribute('href') === 'css/lightTheme.css') {
         themeStylesheet.setAttribute('href', 'css/darkTheme.css');
+
+        // Play the video and show it
+        video.style.display = 'block'; 
+        video.play();
+
+        video.onended = function() {
+            video.style.display = 'none';
+        };
     } else {
         themeStylesheet.setAttribute('href', 'css/lightTheme.css');
+
+        // Hide the image when switching back to light mode
+        image.style.display = 'none';
     }
 }
-
